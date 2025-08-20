c077c3425bc03358e0ff31d8011ca1253bdb6a93
tchakounteingrid <stellatchaks>
2025-08-20 17:01:31 +0100

    Ingrids commit

--- src/com/inventory/dao/DBConnection.java
+++ src/com/inventory/dao/DBConnection.java
@@ -1,4 +1,16 @@
+
 package com.inventory.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL  = "jdbc:postgresql://localhost:5432/inventorydb"; // <-- your DB name
+    private static final String USER = "postgres";   // <-- your username
+    private static final String PASS = "ingrid"; // <-- your password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
